dbcb7552167a7876ffac26e385be59316a1d5f13
dmitrii <dmitrii>
2025-10-16 11:28:42 +0300

    redundant code removed

--- bot/__main__.py
+++ bot/__main__.py
@@ -7,12 +7,6 @@ from bot.handlers.message_echo import MessageEcho
 from bot.handlers.photo_echo import PhotoEcho
 from bot.long_polling import start_long_polling
 
-def get_next_offset(updates: dict) -> int:
-    next_offset = 0
-    for update in updates:
-        next_offset = max(next_offset, update["update_id"] + 1)
-    return next_offset
-
 def main() -> None:
     try:
         dispatcher = Dispatcher()
