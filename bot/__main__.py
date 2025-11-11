51546df8f818bb1c9173d0d839ce51d43e21f997
dima-dimka04 <dima-dimka04@yandex.ru>
2025-11-11 21:49:02 +0300

    postgres and docker done

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,13 +2,14 @@ from bot.dispatcher import Dispatcher
 from bot.handlers import get_handlers
 from bot.long_polling import start_long_polling
 from bot.infrastructure.messenger_telegram import MessengerTelegram
-from bot.infrastructure.storage_sqlite import StorageSqlite
+from bot.infrastructure.storage_postgres import StoragePostgres
 
 
 def main() -> None:
     try:
-        storage = StorageSqlite()
-        storage.recreate_database()
+        #storage = StorageSqlite()
+        storage = StoragePostgres()
+        #storage.recreate_database()
         messenger = MessengerTelegram()
         dispatcher = Dispatcher(storage, messenger)
         dispatcher.add_handler(*get_handlers())
--- bot/long_polling.py
+++ bot/long_polling.py
@@ -11,5 +11,3 @@ def start_long_polling(dispatcher: Dispatcher, messenger: Messenger) -> None:
             next_update_offset = max(next_update_offset, update["update_id"] + 1)
             dispatcher.dispatch(update)
             print(".", end="", flush=True)
-
-        time.sleep(1)
